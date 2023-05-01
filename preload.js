0c5bd30fd55923a11d94b3919aefa7a6f6c4b5b0
nick-812 <nik.jeromel@student.um.si>
2023-05-01 13:15:11 +0200

    Dodan seznam psov

--- preload.js
+++ preload.js
@@ -1,6 +1,16 @@
 const axios = require('axios');
-
 const { contextBridge, ipcRenderer, remote } = require('electron');
+const dexie = require('dexie');
+
+
+//database psov
+const db = new dexie('localDB');
+db.version(1).stores({
+	pets: '_id, name, date_of_birth, animal_id, user_id', // Primary key and indexed props
+	newPets: 'name, _id, date_of_birth, animal_id, user_id',
+});
+localStorage.setItem("db", db);
+
 
 
 contextBridge.exposeInMainWorld('api', {
@@ -10,19 +20,6 @@ contextBridge.exposeInMainWorld('api', {
 	notificationApi: {
 		sendNotification(message) {
 		  ipcRenderer.send('notify', message);
-		}
-	},
-
-	store: {
-		set(message) {
-			console.log(message);
-			global.token = message;
-			console.log(global.token);
-		},
-		get(){
-			const nekaj = remote.getGlobal( "token" );
-			console.log(global.token);
-			return "global.token";
 		}
 	},
 
@@ -53,6 +50,45 @@ contextBridge.exposeInMainWorld('api', {
 				console.log(error.response); // this is the main part. Use the response property from the error object
 				return {status: 400};
 			}		
+		}
+	},
+
+	fetch: {
+		async getAllDogs(){
+
+			const token = localStorage.getItem("token");
+			axios.defaults.headers.common = {'Authorization': `bearer ${token}`};
+
+			//fetchanje novih petov iz strežnika
+			const response = await axios.get('http://localhost:3004/api/getAllPets', {
+				params: {
+					id: token
+				}, 
+			});
+			for (const pet of response.data) {
+				await db.pets.put({
+					_id: pet._id,
+					name: pet.name,
+					date_of_birth: pet.date_of_birth,
+					animal_id: pet.animal_id,
+					user_id: pet.user_id
+				});
+			}
+
+			const oldPets = await db.pets.toArray();
+
+			console.log(oldPets[0].name);
+
+			localStorage.setItem("oldPets", JSON.stringify(oldPets));
+
+			/*
+			db1 = localStorage.getItem("db");
+			const oldPets = await db1.pets.toArray();
+			console.log(oldPets[0].name);
+			*/
+	
+			console.log("Uploaded and refreshed")
+	
 		}
 	}
 })
--- src/psi/psi.js
+++ src/psi/psi.js
@@ -1,10 +1,40 @@
 
 
 const notification_btn = document.getElementById('notify');
-
+const refresh_btn = document.getElementById('fetch_dogs');
+const list = document.getElementById('dogs_list');
 
 
 notification_btn.addEventListener('click', async () => {
 	api.notificationApi.sendNotification('My custom notification! '+localStorage.getItem("token"));
+})
 
+refresh_btn.addEventListener('click', async () => {
+	console.log("refresh");
+
+	await api.fetch.getAllDogs();
+	
+	const oldPets = JSON.parse(localStorage.getItem("oldPets"));
+
+	const dodajPet = (pet) => {
+		const newPet = document.createElement("div");
+		newPet.className = "dog";
+
+		newPet.innerHTML = `<div class="dog">
+			<div class="name_div"><p class="dog_name">${pet.name}</p></div>
+			<div class="labels">
+				<div class="birthday_label_div"><p class="birthday_label">Rojstni datum</p></div>
+				<div class="animalID_label_div"><p class="animalID_label">Animal ID</p></div>
+			</div>
+			<div class="values">
+				<div class="birthday_div"><p class="dog_birthday">${pet.date_of_birth}</p></div>
+				<div class="animalID_div"><p class="dog_animalID">${pet.animal_id}</p></div>
+			</div>
+		</div>`
+
+		if (list) list.appendChild (newPet);
+	}
+	for (const pet of oldPets) {
+		dodajPet(pet);
+	}
 })
